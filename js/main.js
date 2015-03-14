c24b15fefb83725dcee90ac1bd08b8ffa54c5939
Nick Badagliacca <nick@terraeclipse.com>
2015-03-14 14:29:29 -0700

    Initial upload

---
+++ js/main.js
@@ -0,0 +1,22 @@
+$(function() {
+  
+  console.log('JS loaded');
+
+  function setStateBackground(loopNumber, cardSelector) {
+    var $card = $(cardSelector);
+
+    $card.css({ 'background-image': 'url("http://action.fixmedicarenow.org.s3.amazonaws.com/states/' + variables.state + '/' + variables.state + '-' + loopNumber + '.jpg")'});
+  }
+
+  function setLoopNumber(variantArray, selector) {
+    var text = document.getElementsByTagName('h1')[0].innerText
+      , loopNumber = variantArray.length + 1;
+
+    $(selector).addClass('loop-' + loopNumber);
+    setStateBackground(loopNumber, '.card');
+    setFontSize(text, '.card-text', loopNumber);
+  }
+  
+  setLoopNumber(variables.variants_shared, 'body');
+
+});
