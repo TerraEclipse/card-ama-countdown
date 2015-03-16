9c6976af23ffa55728523466c24fb0454b0a818e
Nick Badagliacca <nick@terraeclipse.com>
2015-03-15 21:44:24 -0700

    Undo

--- js/main.js
+++ js/main.js
@@ -13,7 +13,7 @@ $(function() {
   }
 
   function setLoopNumber(variantArray, selector) {
-    var loopNumber = variantArray.length;
+    var loopNumber = variantArray.length + 1;
 
     $(selector).addClass('loop-' + loopNumber);
     setStateBackground(loopNumber, '.card');
